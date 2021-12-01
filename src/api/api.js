95ec9c689ad6d662f439956070c04ef6e710546a
Metsanpeitto <abraham@waldenberginc.com>
2021-12-01 22:08:49 +0100

    Fix linters

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,20 @@
 import axios from 'axios';
 
-// export const getGreetings = async () => {
-//    const response = await fetch("api/v1/greetings");
-
-//    return response.json();
-// };
-
-const getGreetings = async () => axios.get('http://localhost:3000/api/v1/greetings').then((result) => {
+const getGreetings = async () => {
   const greetings = [];
-  if (result.status === 200) {
-    const { data } = result;
-    // eslint-disable-next-line no-restricted-syntax
-    data.forEach((item) => {
-      greetings.push(item.greeting);
-    });
-  }
-  return greetings;
-});
 
+  await axios.get('http://localhost:3000/api/v1/greetings').then((result) => {
+    if (result.status === 200) {
+      const { data } = result;
+      // eslint-disable-next-line no-restricted-syntax
+      data.forEach((item) => {
+        greetings.push(item.greeting);
+      });
+    }
+    return greetings;
+  });
+  return greetings;
+};
 export default {
   getGreetings,
 };
